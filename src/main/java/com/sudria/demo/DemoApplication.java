a2d7a1e92fa9b9315e7ad56f926b2e144aa866f3
Mohamed SLIMANI <mohamed.slimani@nielsen.com>
2020-05-03 14:03:55 +0200

    tp-2 completed

--- src/main/java/com/sudria/demo/DemoApplication.java
+++ src/main/java/com/sudria/demo/DemoApplication.java
@@ -1,51 +1,16 @@
 package com.sudria.demo;
 
-import com.sudria.demo.infrastructure.AnimalEntity;
-import com.sudria.demo.infrastructure.ZooRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
-  @Autowired
-  private ZooRepository zooRepository;
-
-  public DemoApplication(ZooRepository zooRepository) {
-    this.zooRepository = zooRepository;
-  }
+public class DemoApplication  {
 
   public static void main(String[] args) {
 
     SpringApplication.run(DemoApplication.class, args);
     System.out.println("Hello SUDRIA !");
   }
-
-  @Override
-  public void run(String... args) {
-
-    log.info("Data initilisation...");
-    this.zooRepository.save(
-        AnimalEntity
-            .builder()
-            .id(1L)
-            .name("Garfield")
-            .age(5)
-            .category("FELINE")
-            .build());
-
-    this.zooRepository.save(
-        AnimalEntity
-            .builder()
-            .id(2L)
-            .name("Nemo")
-            .age(1)
-            .category("FISCH")
-            .build());
-  }
-
 }
--- src/main/java/com/sudria/demo/application/Controller.java
+++ src/main/java/com/sudria/demo/application/Controller.java
@@ -1,18 +1,8 @@
 package com.sudria.demo.application;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.fge.jsonpatch.JsonPatch;
-import com.github.fge.jsonpatch.JsonPatchException;
-import com.sudria.demo.domain.AnimalService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import java.util.Arrays;
 import java.util.List;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,74 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
-@Api(value = "Zoo Management System", description = "Operations availables for zoo management")
 public class Controller {
 
-
-  private AnimalService animalService;
-  private ObjectMapper objectMapper;
-
-  public Controller(AnimalService animalService,
-      ObjectMapper objectMapper) {
-    this.animalService = animalService;
-    this.objectMapper = objectMapper;
+  public Controller() {
   }
 
-  @ApiOperation(value = "View a list of available animals", response = List.class)
-  @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "Successfully retrieved list"),
-      @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-      @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-      @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-  })
   @RequestMapping(value = "/animals", method = RequestMethod.GET)
-  public ResponseEntity<List<AnimalDto>> getAnimals() {
-    return new ResponseEntity<>(animalService.getAnimals(), HttpStatus.OK);
+  public ResponseEntity<List<String>> getAnimals() {
+    List<String> animals = Arrays.asList("cat", "dog", "turtle");
+    return new ResponseEntity<>(animals, HttpStatus.OK);
   }
 
   @RequestMapping(value = "/animals", method = RequestMethod.POST)
-  public ResponseEntity<AnimalDto> createAnimals(
-      @ApiParam(value = "Animal object store in database table", required = true)
-      @RequestBody AnimalDto animalDto) {
-    animalService.addAnimal(animalDto);
-    return new ResponseEntity<>(animalDto, HttpStatus.CREATED);
+  public ResponseEntity<String> createAnimals(
+      @RequestBody String animalName) {
+    log.info("animal created :" + animalName);
+    return new ResponseEntity<>(animalName, HttpStatus.CREATED);
   }
 
   @RequestMapping(value = "/animals/{id}", method = RequestMethod.PUT)
-  public ResponseEntity<AnimalDto> replaceAnimals(
+  public ResponseEntity<String> replaceAnimals(
       @PathVariable(value = "id") Long id,
-      @RequestBody AnimalDto animalDto) {
-    animalDto.setId(id);
-    animalService.replaceAnimal(animalDto);
-    return new ResponseEntity<>(animalDto, HttpStatus.OK);
+      @RequestBody String animalName) {
+    log.info("animal with id :" + id + " is now :" + animalName);
+    return new ResponseEntity<>(animalName, HttpStatus.OK);
   }
 
-
   @RequestMapping(value = "/animals/{id}", method = RequestMethod.DELETE)
-  public ResponseEntity<AnimalDto> deleteAnimals(@PathVariable(value = "id") Long id) {
-    animalService.deleteAnimals(id);
+  public ResponseEntity<String> deleteAnimals(@PathVariable(value = "id") Long id) {
+    log.info("animal with id :" + id + " is deleted");
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
-  @RequestMapping(value = "/animals/{id}", method = RequestMethod.PATCH, consumes = "application/json-patch+json")
-  public ResponseEntity<String> patchAnimals(
-      @PathVariable(value = "id") Long id,
-      @RequestBody JsonPatch patch)  {
-    try {
-      animalService.patchAnimals(applyPatchToCustomer(patch, animalService.findAnimal(id)));
-      return new ResponseEntity<>(HttpStatus.OK);
-    } catch (NotFoundException e) {
-      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    } catch (JsonPatchException | JsonProcessingException e) {
-      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-
-  private AnimalDto applyPatchToCustomer(JsonPatch patch, AnimalDto targetAnimal)
-      throws JsonPatchException, JsonProcessingException {
-    JsonNode patched = patch.apply(objectMapper.convertValue(targetAnimal, JsonNode.class));
-    return objectMapper.treeToValue(patched, AnimalDto.class);
-  }
 }
